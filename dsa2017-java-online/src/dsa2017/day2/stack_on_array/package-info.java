edc2eeca58160d569420bd45cb6b438859bdce30
tutrunghieu <tutrunghieu@gmail.com>
2017-05-12 20:38:35 +0700

    Adding

---
+++ dsa2017-java-online/src/dsa2017/day2/stack_on_array/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author henrytu
+ *
+ */
+package dsa2017.day2.stack_on_array;
--- dsa2017-java-online/src/dsa2017/day2/to_check_open_close.java
+++ dsa2017-java-online/src/dsa2017/day2/to_check_open_close.java
@@ -6,7 +6,9 @@ public class to_check_open_close {
 
 	public static void main(String[] args)
 	{
-		String s = "[] { )[] (())) }";
+//		String s = "[] { )[] (())) }";
+//		String s = "({[";
+		String s = ")))";
 		int v = checkOpenClose(s);
 		System.out.println("error=" + v);
 	}
@@ -15,32 +17,35 @@ public class to_check_open_close {
 	{
 		s = s.replaceAll("\\s+", "");
 		
-		Stack<Character> used = new Stack<Character> ();
+		Stack<Character> stack = new Stack<Character> ();
 		
 		int error = -1;
 		for(int k=0; k<s.length(); k++)
 		{
+			if(stack.isEmpty()) { error = s.length() + 1; break; }
+			
 			char ck = s.charAt(k);
-			if(ck=='[') used.push(']');
-			else if(ck=='(') used.push(')');
-			else if(ck=='{') used.push('}');
+			if(ck=='[') stack.push(']');
+			else if(ck=='(') stack.push(')');
+			else if(ck=='{') stack.push('}');
 			else if(ck==']') 
 			{
-				if(ck==used.pop()) System.out.println("matched");
+				if(ck==stack.pop()) System.out.println("matched");
 				else { error = k; break; }
 			}
 			else if(ck==')') 
 			{
-				if(ck==used.pop()) System.out.println("matched");
+				if(ck==stack.pop()) System.out.println("matched");
 				else { error = k; break; }
 			}
 			else if(ck=='}') 
 			{
-				if(ck==used.pop()) System.out.println("matched");
+				if(ck==stack.pop()) System.out.println("matched");
 				else { error = k; break; }
 			}
 		}
 	
+		if(!stack.isEmpty()) return s.length() + 1;
 		return error;
 	}
 
