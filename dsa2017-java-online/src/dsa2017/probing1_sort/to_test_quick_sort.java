079660a77b762ae03c9465825630f8c0532fefcc
tutrunghieu <tutrunghieu@gmail.com>
2017-05-14 17:14:18 +0700

    adding prob1

---
+++ dsa2017-java-online/src/dsa2017/probing1_sort/to_test_quick_sort.java
@@ -0,0 +1,60 @@
+package dsa2017.probing1_sort;
+
+import java.util.Arrays;
+
+public class to_test_quick_sort {
+
+	public static void main(String[] args) 
+	{
+		String[] a = {
+				"Anh", "Nam", "Binh", "Duc", "Cuong", "Hai", "Tuan", "Mai", 
+				"Duc2", "Cuong2"				
+		};
+		
+		quickSort(a);
+		System.out.println("final: " + Arrays.asList(a));
+	}
+
+	public static void quickSort(String[] a) 
+	{
+		quickSort(a, 0, a.length);
+	}
+
+	private static void quickSort(String[] a, int left, int right) 
+	{
+		if(left+1>=right) return;
+		
+		int mid = partition(a, left, right);
+		System.out.println("part "+left+"->"+(right-1)+": " +
+		Arrays.asList(Arrays.copyOfRange(a, left, right)) );
+		
+		quickSort(a, left, mid);
+		quickSort(a, mid+1, right);		
+	}
+
+	private static int partition(String[] a, int left, int right) 
+	{
+		 String pivot = a[right-1];
+	
+		 int i=left-1;
+		 
+		 for(int j=left; j<right-1; j++) 
+		 if(a[j].compareTo(pivot) <= 0) 
+		 {
+			 i++;
+			 swap(a, i, j);
+		 }
+		 
+		swap(a, i+1, right-1);
+	    return i + 1;
+	}
+
+	private static<T1> void swap(T1[] a, int x, int y) 
+	{
+		T1 t = a[x];
+		a[x] = a[y];
+		a[y] = t;
+	}
+	
+
+}
