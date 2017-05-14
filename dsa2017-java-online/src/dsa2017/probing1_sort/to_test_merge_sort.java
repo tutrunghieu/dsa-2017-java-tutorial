03c8de207362c329964c550d08580fa438484b6f
tutrunghieu <tutrunghieu@gmail.com>
2017-05-15 00:09:16 +0700

    Adding merge

--- dsa2017-java-online/src/dsa2017/probing1_sort/to_test_merge_sort.java
+++ dsa2017-java-online/src/dsa2017/probing1_sort/to_test_merge_sort.java
@@ -6,37 +6,48 @@ public class to_test_merge_sort {
 
 	public static void main(String[] args) 
 	{
+//		String[] a1 = { "Anh", "Cuong", "Cuong2", "Trung" };
+//		String[] a2 = { "Binh", "Cuong", "Xuan" };
+//		
+//		System.out.println(Arrays.asList(merge(a1, a2)));
+		
+		
 		String[] a = {
-				"Anh", "Nam", "Binh", "Duc", "Cuong", "Hai", "Tuan", "Mai", 
-				"Duc2", "Cuong2"				
+				"Anh", "Nam", "Binh", "Duc", 
+				"Cuong", "Hai", "Tuan", "Mai", 
+				"Duc2", "Cuong2", "Lam"				
 		};
 		
-		mergeSort(a);
+		a = mergeSort(a, 0, a.length);
 		System.out.println("final: " + Arrays.asList(a));
 	}
 
-	public static void mergeSort(String[] a) 
+	public static String[] mergeSort(String[] a, int left, int right) 
 	{
-		mergeSort(a, 0, a.length);
+		if(right-left > 2) 
+		{
+			int mid = (left+right+1)/2;
+			String[] a1 = mergeSort(a, left, mid);
+			String[] a2 = mergeSort(a, mid, right);	
+			
+			System.out.println("a1: " + Arrays.asList(a1));
+			System.out.println("a2: " + Arrays.asList(a2));
+			return merge(a1, a2);
+		}
+		
+		else 
+		{
+			a = Arrays.copyOfRange(a, left, right);
+			if(a.length==2 && a[0].compareTo(a[1]) > 0) SortUtils.swap(a, 0, 1);
+			return a;
+		}
+				
 	}
 
-	private static String[] mergeSort(String[] a, int left, int right) 
+	private static String[] merge(String[] a1, String[] a2) 
 	{
-		if(left >= right) return a;
-		
-		int mid = (left+right)/2;
-		String[] a1 = mergeSort(a, left, mid);
-		String[] a2 = mergeSort(a, mid+1, right);	
-		
-		return merge(a, left, right, left, mid, mid+1, right);
-	}
-
-	private static String[] merge(String[] a, int left, int right, 
-			int left1, int right1, int left2, int right2) 
-	{
-//		int left = 0, left1 = 0, left2 = 0, right1 = a1.length, right2 = a2.length;
-//		String[] a = new String[right1 + right2];
-		String[] a1 = a, a2 = a;
+		int left = 0, left1 = 0, left2 = 0, right1 = a1.length, right2 = a2.length;
+		String[] a = new String[right1 + right2];
 		
 		for(; left1<right1 && left2<right2; )
 		{
