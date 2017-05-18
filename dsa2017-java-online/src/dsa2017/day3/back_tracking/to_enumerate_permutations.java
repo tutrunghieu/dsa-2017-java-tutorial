9e2c2b32e0bc50d97bfb1798ce06df2273924405
henrytu <tutrunghieu@gmail.com>
2017-05-18 11:36:00 +0700

    Adding enum

---
+++ dsa2017-java-online/src/dsa2017/day3/back_tracking/to_enumerate_permutations.java
@@ -0,0 +1,47 @@
+package dsa2017.day3.back_tracking;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class to_enumerate_permutations {
+
+	public static void main(String[] args) 
+	{
+		Set<String> avail = new LinkedHashSet<String>(Arrays.asList("Amy", "Frank", "Dave", "Lucas"));
+		enumerate(avail);
+	}
+	
+	private static void enumerate(Set<String> avail) 
+	{
+		String[] conf = new String[avail.size()]; 
+		enumerate(0, conf, avail);		
+	}
+
+	private static void enumerate(int n, String[] conf, Set<String> avail) 
+	{
+		for(String ak: avail) 
+		{
+			conf[n] = ak;
+			
+			if(n+1==conf.length) {
+				System.out.println(Arrays.asList(conf));
+			}
+			else {
+				Set<String> avail1 = remove(avail, ak);
+				enumerate(n+1, conf, avail1);
+			}
+		}
+		
+		return;
+	}
+
+	private static Set<String> remove(Set<String> avail, String ak) 
+	{
+		Set<String> res = new LinkedHashSet<String>();
+		res.addAll(avail);
+		res.remove(ak);
+		return res;
+	}
+
+}
