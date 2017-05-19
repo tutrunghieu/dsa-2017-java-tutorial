2c696861919f17c27b10c577eee539bf19cd2a2d
tutrunghieu <tutrunghieu@gmail.com>
2017-05-19 21:00:38 +0700

    Adding gradle

---
+++ dsa2017-project1/src/org/tests/readfiles/to_read_customer_file.java
@@ -0,0 +1,13 @@
+package org.tests.readfiles;
+
+public class to_read_customer_file {
+
+	//xem huong dan tai
+	// https://docs.google.com/document/d/1jG4vWu3Bf-WjjAnQSlR7eAoMjxVMPhrh1Xycv1fihRc/edit?usp=sharing
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
