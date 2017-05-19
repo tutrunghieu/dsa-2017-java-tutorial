81699bd9551f639067c008f2507dc93ce268bab9
tutrunghieu <tutrunghieu@gmail.com>
2017-05-19 23:23:13 +0700

    Adding radix

--- dsa2017-java-online/src/dsa2017/day4/effective_sort/to_test_radix_sort.java
+++ dsa2017-java-online/src/dsa2017/day4/effective_sort/to_test_radix_sort.java
@@ -11,22 +11,26 @@ public class to_test_radix_sort {
 
 	public static void main(String[] args) 
 	{
-		List<String> a 
-		  = new ArrayList<String>(Arrays.asList(
-				  "a2b", "a1b", "bc2", 
-				  "bc1", "de1", "df2"));
+		List<String> a = new ArrayList<String>(Arrays.asList(
+				  "123", "134", "357", 
+				  "436", "840", "368"));
 		
 		radixSort(a);
 	}
 
 	private static void radixSort(List<String> a)
 	{
-		Map<Character, List<String>> m = stepFirst(a, 0);
-		m = stepFollow(m, 1);
-		m = stepFollow(m, 2);		
+		a = step(a, 2); 
+		System.out.println("step 0: " + a);
+		
+		a = step(a, 1);
+		System.out.println("step 1: " + a);
+		
+		a = step(a, 0);
+		System.out.println("step 2: " + a);
 	}
 
-	private static Map<Character, List<String>> stepFirst(List<String> a, int d) 
+	private static List<String> step(List<String> a, int d) 
 	{
 		Map<Character, List<String>> m = new TreeMap<>();
 		
@@ -42,27 +46,14 @@ public class to_test_radix_sort {
 		}
 		
 		System.out.println(m);		
-		return m;
+		return collect(m);
 	}
 
-	private static Map<Character, List<String>> stepFollow(Map<Character, List<String>> m1, int d) 
+	private static List<String> collect(Map<Character, List<String>> m) 
 	{
-		Map<Character, List<String>> m = new LinkedHashMap<>();
-		
-		for(Character t: m1.keySet()) 
-		for(String ak: m1.get(t)) 
-		{
-			char c0 = ak.charAt(d);
-			List<String> l0 = m.get(c0);
-			
-			if(l0==null) 
-				m.put(c0, l0 = new ArrayList<String>());
-						
-			l0.add(ak);
-		}
-		
-		System.out.println(m);
-		return m;
+		List<String> res = new ArrayList<String>();
+		for(Character t: m.keySet()) res.addAll(m.get(t));
+		return res;
 	}
 
 }
