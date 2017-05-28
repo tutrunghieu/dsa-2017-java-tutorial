a99af4e843443052420b6064344f93a9e216f307
tutrunghieu <tutrunghieu@gmail.com>
2017-05-28 14:15:01 +0700

    adding msd

---
+++ dsa2017-java-online/src-probing/dsa2017/probing7_countsort/to_test_countsort_msd.java
@@ -0,0 +1,35 @@
+package dsa2017.probing7_countsort;
+
+import java.util.Arrays;
+
+public class to_test_countsort_msd {
+
+	public static void main(String[] args) 
+	{
+		String[] a = { "BEG", "ABC", "DCA", "CDB", "ADC", "BCD", "BEF" };
+		
+		a = radixSort(a, 2); System.out.println(Arrays.asList(a));
+		a = radixSort(a, 1); System.out.println(Arrays.asList(a));
+		a = radixSort(a, 2); System.out.println(Arrays.asList(a));
+	}
+
+	private static String[] radixSort(String[] a, int d) 
+	{
+		int[] C = new int['G'-'A' + 1];
+		int[] S = new int['G'-'A' + 1];
+		
+		for(String ak: a) { int j = radix(ak, d); C[j]++; }
+		for(int j=1; j<C.length; j++) S[j] = S[j-1] + C[j-1];
+		
+		String[] res = new String[a.length];
+		for(String ak: a) { int j = radix(ak, d); res[ S[j]++ ] = ak; }
+		
+		return res;
+	}
+
+	private static int radix(String ak, int d)
+	{
+		return ak.charAt(d) - 'A';
+	}
+
+}
